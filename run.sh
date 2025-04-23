f53f90200d1d3db63a404acbcd0c869d2f90ec6a
ioeshx <228753522@qq.com>
2025-04-23 14:05:25 +0800

    modify_3 done

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ current_time=$(date +"%m-%d_%H-%M")
 
 nohup python main_HTNet.py \
 --epochs 400 \
---train True > logs/output_${current_time}.log
+--train True > logs/output_${current_time}_modify_3.log
