e767384f53914ac9729eef3ba19e04d8258d45a7
ioeshx <228753522@qq.com>
2025-05-01 13:26:00 +0800

    using origin

--- main_HTNet.py
+++ main_HTNet.py
@@ -51,34 +51,50 @@ def reset_weights(m):  # Reset the weights for network to avoid weight leakage
             layer.reset_parameters()
 
 def confusionMatrix(gt, pred, show=False):
-    TN, FP, FN, TP = confusion_matrix(gt, pred).ravel()
+    TN, FP, FN, TP = confusion_matrix(gt, pred, labels=[0,1]).ravel()
+    # TN, FP, FN, TP = confusion_matrix(gt, pred).ravel()
     f1_score = (2 * TP) / (2 * TP + FP + FN)
     num_samples = len([x for x in gt if x == 1])
     average_recall = TP / num_samples
     return f1_score, average_recall
-
 
 def recognition_evaluation(final_gt, final_pred, show=False):
     label_dict = {'negative': 0, 'positive': 1, 'surprise': 2}
     # Display recognition result
     f1_list = []
     ar_list = []
-    try:
-        for emotion, emotion_index in label_dict.items():
-            gt_recog = [1 if x == emotion_index else 0 for x in final_gt]
-            pred_recog = [1 if x == emotion_index else 0 for x in final_pred]
-            try:
-                f1_recog, ar_recog = confusionMatrix(gt_recog, pred_recog)
-                f1_list.append(f1_recog)
-                ar_list.append(ar_recog)
-            except Exception as e:
-                print("recognition evaluation error!")
-                sys.exit()
-        UF1 = np.mean(f1_list)
-        UAR = np.mean(ar_list)
-        return UF1, UAR
-    except:
-        return '', ''
+    
+    for emotion, emotion_index in label_dict.items():
+        gt_recog = [1 if x == emotion_index else 0 for x in final_gt]
+        pred_recog = [1 if x == emotion_index else 0 for x in final_pred]
+        f1_recog, ar_recog = confusionMatrix(gt_recog, pred_recog)
+        f1_list.append(f1_recog)
+        ar_list.append(ar_recog)
+    UF1 = np.mean(f1_list)
+    UAR = np.mean(ar_list)
+    return UF1, UAR
+
+# def recognition_evaluation(final_gt, final_pred, show=False):
+#     label_dict = {'negative': 0, 'positive': 1, 'surprise': 2}
+#     # Display recognition result
+#     f1_list = []
+#     ar_list = []
+#     try:
+#         for emotion, emotion_index in label_dict.items():
+#             gt_recog = [1 if x == emotion_index else 0 for x in final_gt]
+#             pred_recog = [1 if x == emotion_index else 0 for x in final_pred]
+#             try:
+#                 f1_recog, ar_recog = confusionMatrix(gt_recog, pred_recog)
+#                 f1_list.append(f1_recog)
+#                 ar_list.append(ar_recog)
+#             except Exception as e:
+#                 print("recognition evaluation error!")
+#                 sys.exit()
+#         UF1 = np.mean(f1_list)
+#         UAR = np.mean(ar_list)
+#         return UF1, UAR
+#     except:
+#         return '', ''
 
 # 1. get the whole face block coordinates
 def whole_face_block_coordinates():
@@ -250,7 +266,7 @@ def main(config):
         weight_path = WEIGHT_DIR + '/' + n_subName + '.pth'
 
         # Reset or load model weigts
-        model = HTNet_Enhanced(
+        model = HTNet(
             image_size=28,
             patch_size=7,
             dim=256,  # 256,--96, 56-, 192
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 current_time=$(date +"%m-%d_%H-%M")
 
 nohup python main_HTNet.py \
---epochs 400 \
+--epochs 800 \
 --train True > logs/output_${current_time}.log
